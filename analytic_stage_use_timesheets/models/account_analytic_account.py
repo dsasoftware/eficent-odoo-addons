a86997c471c59678a9e7c746f8b8e4039d801f79
ahenriquez <ahenriquez@eficent.com>
2016-10-25 16:27:17 +0200

    [FIX] The module analytic_stage_use_timesheets should not change this if the account_class is Phase or deliverable

--- analytic_stage_use_timesheets/models/account_analytic_account.py
+++ analytic_stage_use_timesheets/models/account_analytic_account.py
@@ -9,14 +9,19 @@ class AccountAnalyticAccount(orm.Model):
     _inherit = 'account.analytic.account'
 
     def write(self, cr, uid, ids, values, context=None):
-        if values.get('stage_id', False):
-            stage_obj = self.pool['analytic.account.stage']
-            stage = stage_obj.browse(cr, uid, values['stage_id'],
-                                     context=context)
-            if stage.use_timesheets:
-                values['use_timesheets'] = True
-            else:
+        for project_id in ids:
+            project = self.browse(cr, uid, project_id)
+            if values.get('stage_id', False):
+                stage_obj = self.pool['analytic.account.stage']
+                stage = stage_obj.browse(cr, uid, values['stage_id'],
+                                         context=context)
+                if stage.use_timesheets and project.account_class not in\
+                        ('phase', 'deriverable'):
+                    values['use_timesheets'] = True
+                else:
+                    values['use_timesheets'] = False
                 values['use_timesheets'] = False
+
         return super(AccountAnalyticAccount, self).write(cr, uid, ids,
                                                          values,
                                                          context=context)
